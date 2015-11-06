1020ebbd3a3a039641b33bc8839e2ed7d4fc40fe
Jared Allard <jaredallard@outlook.com>
2015-11-06 10:47:07 -0800

    use custom BiOS

--- bios.lua
+++ bios.lua
@@ -1,97 +1,11 @@
+print("CBios v1.0.0")
+print("CrfOS v1.7.4")
+print("Node "..tostring(njs.host_emu_version()))
+print("OpenSSL v"..tostring(njs.host_openssl_version()))
 
 local nativesetfenv = setfenv
+local nativeprint = print
 local nativegetfenv = getfenv
-if _VERSION == "Lua 5.1" then
-    -- Install parts of the Lua 5.2 API so that programs can be written against it now
-    local nativeload = load
-    local nativeloadstring = loadstring
-    function load( x, name, mode, env )
-        if mode ~= nil and mode ~= "t" then
-            error( "Binary chunk loading prohibited", 2 )
-        end
-        local ok, p1, p2 = pcall( function()        
-            if type(x) == "string" then
-                local result, err = nativeloadstring( x, name )
-                if result then
-                    if env then
-                        env._ENV = env
-                        nativesetfenv( result, env )
-                    end
-                    return result
-                else
-                    return nil, err
-                end
-            else
-                local result, err = nativeload( x, name )
-                if result then
-                    if env then
-                        env._ENV = env
-                        nativesetfenv( result, env )
-                    end
-                    return result
-                else
-                    return nil, err
-                end
-            end
-        end )
-        if ok then
-            return p1, p2
-        else
-            error( p1, 2 )
-        end        
-    end
-    table.unpack = unpack
-    table.pack = function( ... ) return { ... } end
-
-    local nativebit = bit
-    bit32 = {}
-    bit32.arshift = nativebit.brshift
-    bit32.band = nativebit.band
-    bit32.bnot = nativebit.bnot
-    bit32.bor = nativebit.bor
-    bit32.btest = function( a, b ) return nativebit.band(a,b) ~= 0 end
-    bit32.bxor = nativebit.bxor
-    bit32.lshift = nativebit.blshift
-    bit32.rshift = nativebit.blogic_rshift
-
-    if _CC_DISABLE_LUA51_FEATURES then
-        -- Remove the Lua 5.1 features that will be removed when we update to Lua 5.2, for compatibility testing.
-        -- See "disable_lua51_functions" in ComputerCraft.cfg
-        setfenv = nil
-        getfenv = nil
-        loadstring = nil
-        unpack = nil
-        math.log10 = nil
-        table.maxn = nil
-        bit = nil
-    end
-end
-
--- Install fix for luaj's broken string.sub/string.find
-do
-    local nativestringfind = string.find
-    local nativestringsub = string.sub
-    local nativepcall = pcall
-    local stringCopy = {}
-    for k,v in pairs(string) do
-        stringCopy[k] = v
-    end
-    stringCopy.sub = function( s, start, _end )
-        local ok, r = nativepcall( nativestringsub, s, start, _end )
-        if ok then
-            if r then
-                return r .. ""
-            end
-            return nil
-        else
-            error( r, 2 )
-        end
-    end
-    stringCopy.find = function( s, ... )
-        return nativestringfind( s .. "", ... );
-    end
-    string = stringCopy
-end
 
 -- Prevent access to metatables or environments of strings, as these are global between all computers
 do
@@ -105,23 +19,6 @@ do
             nativeerror( "Attempt to access string metatable", 2 )
         else
             return mt
-        end
-    end
-    if _VERSION == "Lua 5.1" and not _CC_DISABLE_LUA51_FEATURES then
-        local string_env = nativegetfenv(("").gsub)
-        function getfenv( env )
-            if env == nil then
-                env = 2
-            elseif nativetype( env ) == "number" and env > 0 then
-                env = env + 1
-            end
-            local fenv = nativegetfenv(env)
-            if fenv == string_env then
-                --nativeerror( "Attempt to access string metatable", 2 )
-                return nativegetfenv( 0 )
-            else
-                return fenv
-            end
         end
     end
 end
@@ -152,349 +49,20 @@ function sleep( nTime )
 end
 
 function write( sText )
-    local w,h = term.getSize()        
-    local x,y = term.getCursorPos()
-    
-    local nLinesPrinted = 0
-    local function newLine()
-        if y + 1 <= h then
-            term.setCursorPos(1, y + 1)
-        else
-            term.setCursorPos(1, h)
-            term.scroll(1)
-        end
-        x, y = term.getCursorPos()
-        nLinesPrinted = nLinesPrinted + 1
-    end
-    
-    -- Print the line with proper word wrapping
-    while string.len(sText) > 0 do
-        local whitespace = string.match( sText, "^[ \t]+" )
-        if whitespace then
-            -- Print whitespace
-            term.write( whitespace )
-            x,y = term.getCursorPos()
-            sText = string.sub( sText, string.len(whitespace) + 1 )
-        end
-        
-        local newline = string.match( sText, "^\n" )
-        if newline then
-            -- Print newlines
-            newLine()
-            sText = string.sub( sText, 2 )
-        end
-        
-        local text = string.match( sText, "^[^ \t\n]+" )
-        if text then
-            sText = string.sub( sText, string.len(text) + 1 )
-            if string.len(text) > w then
-                -- Print a multiline word                
-                while string.len( text ) > 0 do
-                    if x > w then
-                        newLine()
-                    end
-                    term.write( text )
-                    text = string.sub( text, (w-x) + 2 )
-                    x,y = term.getCursorPos()
-                end
-            else
-                -- Print a word normally
-                if x + string.len(text) - 1 > w then
-                    newLine()
-                end
-                term.write( text )
-                x,y = term.getCursorPos()
-            end
-        end
-    end
-    
-    return nLinesPrinted
+  njs.write(sText)
 end
 
 function print( ... )
-    local nLinesPrinted = 0
-    for n,v in ipairs( { ... } ) do
-        nLinesPrinted = nLinesPrinted + write( tostring( v ) )
-    end
-    nLinesPrinted = nLinesPrinted + write( "\n" )
-    return nLinesPrinted
+  nativeprint(...)
 end
 
 function printError( ... )
-    if term.isColour() then
-        term.setTextColour( colors.red )
-    end
     print( ... )
-    if term.isColour() then
-        term.setTextColour( colors.white )
-    end
 end
 
 function read( _sReplaceChar, _tHistory, _fnComplete )
-    term.setCursorBlink( true )
+    sLine = njs.read()
 
-    local sLine = ""
-    local nHistoryPos
-    local nPos = 0
-    if _sReplaceChar then
-        _sReplaceChar = string.sub( _sReplaceChar, 1, 1 )
-    end
-
-    local tCompletions
-    local nCompletion
-    local function recomplete()
-        if _fnComplete and nPos == string.len(sLine) then
-            tCompletions = _fnComplete( sLine )
-            if tCompletions and #tCompletions > 0 then
-                nCompletion = 1
-            else
-                nCompletion = nil
-            end
-        else
-            tCompletions = nil
-            nCompletion = nil
-        end
-    end
-
-    local function uncomplete()
-        tCompletions = nil
-        nCompletion = nil
-    end
-
-    local w = term.getSize()
-    local sx = term.getCursorPos()
-
-    local function redraw( _bClear )
-        local nScroll = 0
-        if sx + nPos >= w then
-            nScroll = (sx + nPos) - w
-        end
-
-        local cx,cy = term.getCursorPos()
-        term.setCursorPos( sx, cy )
-        local sReplace = (_bClear and " ") or _sReplaceChar
-        if sReplace then
-            term.write( string.rep( sReplace, math.max( string.len(sLine) - nScroll, 0 ) ) )
-        else
-            term.write( string.sub( sLine, nScroll + 1 ) )
-        end
-
-        if nCompletion then
-            local sCompletion = tCompletions[ nCompletion ]
-            local oldText, oldBg
-            if not _bClear then
-                oldText = term.getTextColor()
-                oldBg = term.getBackgroundColor()
-                term.setTextColor( colors.white )
-                term.setBackgroundColor( colors.gray )
-            end
-            if sReplace then
-                term.write( string.rep( sReplace, string.len( sCompletion ) ) )
-            else
-                term.write( sCompletion )
-            end
-            if not _bClear then
-                term.setTextColor( oldText )
-                term.setBackgroundColor( oldBg )
-            end
-        end
-
-        term.setCursorPos( sx + nPos - nScroll, cy )
-    end
-    
-    local function clear()
-        redraw( true )
-    end
-
-    recomplete()
-    redraw()
-
-    local function acceptCompletion()
-        if nCompletion then
-            -- Clear
-            clear()
-
-            -- Find the common prefix of all the other suggestions which start with the same letter as the current one
-            local sCompletion = tCompletions[ nCompletion ]
-            local sFirstLetter = string.sub( sCompletion, 1, 1 )
-            local sCommonPrefix = sCompletion
-            for n=1,#tCompletions do
-                local sResult = tCompletions[n]
-                if n ~= nCompletion and string.find( sResult, sFirstLetter, 1, true ) == 1 then
-                    while #sCommonPrefix > 1 do
-                        if string.find( sResult, sCommonPrefix, 1, true ) == 1 then
-                            break
-                        else
-                            sCommonPrefix = string.sub( sCommonPrefix, 1, #sCommonPrefix - 1 )
-                        end
-                    end
-                end
-            end
-
-            -- Append this string
-            sLine = sLine .. sCommonPrefix
-            nPos = string.len( sLine )
-        end
-
-        recomplete()
-        redraw()
-    end
-    while true do
-        local sEvent, param = os.pullEvent()
-        if sEvent == "char" then
-            -- Typed key
-            clear()
-            sLine = string.sub( sLine, 1, nPos ) .. param .. string.sub( sLine, nPos + 1 )
-            nPos = nPos + 1
-            recomplete()
-            redraw()
-
-        elseif sEvent == "paste" then
-            -- Pasted text
-            clear()
-            sLine = string.sub( sLine, 1, nPos ) .. param .. string.sub( sLine, nPos + 1 )
-            nPos = nPos + string.len( param )
-            recomplete()
-            redraw()
-
-        elseif sEvent == "key" then
-            if param == keys.enter then
-                -- Enter
-                if nCompletion then
-                    clear()
-                    uncomplete()
-                    redraw()
-                end
-                break
-                
-            elseif param == keys.left then
-                -- Left
-                if nPos > 0 then
-                    clear()
-                    nPos = nPos - 1
-                    recomplete()
-                    redraw()
-                end
-                
-            elseif param == keys.right then
-                -- Right                
-                if nPos < string.len(sLine) then
-                    -- Move right
-                    clear()
-                    nPos = nPos + 1
-                    recomplete()
-                    redraw()
-                else
-                    -- Accept autocomplete
-                    acceptCompletion()
-                end
-
-            elseif param == keys.up or param == keys.down then
-                -- Up or down
-                if nCompletion then
-                    -- Cycle completions
-                    clear()
-                    if param == keys.up then
-                        nCompletion = nCompletion - 1
-                        if nCompletion < 1 then
-                            nCompletion = #tCompletions
-                        end
-                    elseif param == keys.down then
-                        nCompletion = nCompletion + 1
-                        if nCompletion > #tCompletions then
-                            nCompletion = 1
-                        end
-                    end
-                    redraw()
-
-                elseif _tHistory then
-                    -- Cycle history
-                    clear()
-                    if param == keys.up then
-                        -- Up
-                        if nHistoryPos == nil then
-                            if #_tHistory > 0 then
-                                nHistoryPos = #_tHistory
-                            end
-                        elseif nHistoryPos > 1 then
-                            nHistoryPos = nHistoryPos - 1
-                        end
-                    else
-                        -- Down
-                        if nHistoryPos == #_tHistory then
-                            nHistoryPos = nil
-                        elseif nHistoryPos ~= nil then
-                            nHistoryPos = nHistoryPos + 1
-                        end                        
-                    end
-                    if nHistoryPos then
-                        sLine = _tHistory[nHistoryPos]
-                        nPos = string.len( sLine ) 
-                    else
-                        sLine = ""
-                        nPos = 0
-                    end
-                    uncomplete()
-                    redraw()
-
-                end
-
-            elseif param == keys.backspace then
-                -- Backspace
-                if nPos > 0 then
-                    clear()
-                    sLine = string.sub( sLine, 1, nPos - 1 ) .. string.sub( sLine, nPos + 1 )
-                    nPos = nPos - 1
-                    recomplete()
-                    redraw()
-                end
-
-            elseif param == keys.home then
-                -- Home
-                if nPos > 0 then
-                    clear()
-                    nPos = 0
-                    recomplete()
-                    redraw()
-                end
-
-            elseif param == keys.delete then
-                -- Delete
-                if nPos < string.len(sLine) then
-                    clear()
-                    sLine = string.sub( sLine, 1, nPos ) .. string.sub( sLine, nPos + 2 )                
-                    recomplete()
-                    redraw()
-                end
-
-            elseif param == keys["end"] then
-                -- End
-                if nPos < string.len(sLine ) then
-                    clear()
-                    nPos = string.len(sLine)
-                    recomplete()
-                    redraw()
-                end
-
-            elseif param == keys.tab then
-                -- Tab (accept autocomplete)
-                acceptCompletion()
-
-            end
-
-        elseif sEvent == "term_resize" then
-            -- Terminal resized
-            w = term.getSize()
-            redraw()
-
-        end
-    end
-
-    local cx, cy = term.getCursorPos()
-    term.setCursorBlink( false )
-    term.setCursorPos( w + 1, cy )
-    print()
-    
     return sLine
 end
 
@@ -506,18 +74,6 @@ loadfile = function( _sFile, _tEnv )
         return func, err
     end
     return nil, "File not found"
-end
-
-if _VERSION == "Lua 5.1" and not _CC_DISABLE_LUA51_FEATURES then
-    dofile = function( _sFile )
-        local fnFile, e = loadfile( _sFile )
-        if fnFile then
-            setfenv( fnFile, getfenv(2) )
-            return fnFile()
-        else
-            error( e, 2 )
-        end
-    end
 end
 
 -- Install the rest of the OS api
@@ -568,7 +124,7 @@ function os.loadAPI( _sPath )
         tAPIsLoading[sName] = nil
         return false
     end
-    
+
     local tAPI = {}
     for k,v in pairs( tEnv ) do
         if k ~= "_ENV" then
@@ -576,7 +132,7 @@ function os.loadAPI( _sPath )
         end
     end
 
-    _G[sName] = tAPI    
+    _G[sName] = tAPI
     tAPIsLoading[sName] = nil
     return true
 end
@@ -589,22 +145,6 @@ end
 
 function os.sleep( nTime )
     sleep( nTime )
-end
-
-local nativeShutdown = os.shutdown
-function os.shutdown()
-    nativeShutdown()
-    while true do
-        coroutine.yield()
-    end
-end
-
-local nativeReboot = os.reboot
-function os.reboot()
-    nativeReboot()
-    while true do
-        coroutine.yield()
-    end
 end
 
 -- Install the lua part of the HTTP api (if enabled)
@@ -625,7 +165,7 @@ if http then
         end
         return nil, err
     end
-    
+
     http.get = function( _url, _headers )
         return wrapRequest( _url, nil, _headers )
     end
@@ -716,66 +256,8 @@ for n,sFile in ipairs( tApis ) do
     end
 end
 
-if turtle then
-    -- Load turtle APIs
-    local tApis = fs.list( "rom/apis/turtle" )
-    for n,sFile in ipairs( tApis ) do
-        if string.sub( sFile, 1, 1 ) ~= "." then
-            local sPath = fs.combine( "rom/apis/turtle", sFile )
-            if not fs.isDir( sPath ) then
-                if not os.loadAPI( sPath ) then
-                    bAPIError = true
-                end
-            end
-        end
-    end
-end
-
-if pocket and fs.isDir( "rom/apis/pocket" ) then
-    -- Load pocket APIs
-    local tApis = fs.list( "rom/apis/pocket" )
-    for n,sFile in ipairs( tApis ) do
-        if string.sub( sFile, 1, 1 ) ~= "." then
-            local sPath = fs.combine( "rom/apis/pocket", sFile )
-            if not fs.isDir( sPath ) then
-                if not os.loadAPI( sPath ) then
-                    bAPIError = true
-                end
-            end
-        end
-    end
-end
-
-if commands and fs.isDir( "rom/apis/command" ) then
-    -- Load command APIs
-    if os.loadAPI( "rom/apis/command/commands" ) then
-        -- Add a special case-insensitive metatable to the commands api
-        local tCaseInsensitiveMetatable = {
-            __index = function( table, key )
-                local value = rawget( table, key )
-                if value ~= nil then
-                    return value
-                end
-                if type(key) == "string" then
-                    local value = rawget( table, string.lower(key) )
-                    if value ~= nil then
-                        return value
-                    end
-                end
-                return nil
-            end
-        }
-        setmetatable( commands, tCaseInsensitiveMetatable )
-        setmetatable( commands.async, tCaseInsensitiveMetatable )
-
-        -- Add global "exec" function
-        exec = commands.exec
-    else
-        bAPIError = true
-    end
-end
-
 if bAPIError then
+    print("API ERROR")
     print( "Press any key to continue" )
     os.pullEvent( "key" )
     term.clear()
@@ -784,29 +266,19 @@ end
 
 -- Run the shell
 local ok, err = pcall( function()
-    parallel.waitForAny( 
+    parallel.waitForAny(
         function()
-            if term.isColour() then
-                os.run( {}, "rom/programs/advanced/multishell" )
-            else
-                os.run( {}, "rom/programs/shell" )
-            end
-            os.run( {}, "rom/programs/shutdown" )
+          njs.docode()
         end,
         function()
-            rednet.run()
+          rednet.run()
         end )
 end )
 
 -- If the shell errored, let the user read it.
 term.redirect( term.native() )
 if not ok then
-    printError( err )
-    pcall( function()
-        term.setCursorBlink( false )
-        print( "Press any key to continue" )
-        os.pullEvent( "key" )
-    end )
+  print(err)
 end
 
 -- End
